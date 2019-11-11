0fd479815fd24d0286f2307627d5dcd872c31408
Joseph Aveltsev <aveltsev@gmail.com>
2019-11-12 00:50:05 +0200

    beta

--- lab4asm/Main.asm
+++ lab4asm/Main.asm
@@ -6,57 +6,49 @@ TITLE lab2
 	ExitProcess PROTO, dwExitCode:DWORD
 
 .data
-	a db 10,25,35,17,18,30,15,20,12,1
-	a1 db 10 dup(?)
-	b db 10
-	r dw 5 dup(?)
-	max db -128
+	FirstArr db 53, 55, 87, 85, 82, 78, 49, 39, 98, 4, 95, 38, 41, 81, 53, 89, 10, 74, 40, 2
+	SecondArr db 52, 53, 94, -11, -59, 39, -80, 19, 24, 46, -2, -30, 0, 82, 87, 13, -52, 63, 84, 50, 33, 93, -92, -53, 54, 99, -29, -60, 62, -51
+	ResultArr db 20 dup(?)
+	mindif db 127
 
 .code
 _main proc
-		mov cx, 10
-		mov esi, 0
+	comment *дано два целочисленных массива длиной 20 и 30 элементов
+	соответственно. для каждого элемента первого массива определить самый 
+	близкий элемент второго массива, т. е. такой, для которого значение
+	|a[i] - b[j]| минимально.*
 
-	begin:
-		mov al, a[si]
-		sub al, b
-		cmp al, 0
-		jg n1
-		neg al
-	n1:
-		mov a1[si], al
-		inc si
-		loop begin
+	mov ecx, 20
+	mov esi, 0
 
-
-		mov bx, 0
-		mov cx, 5
-	cycle:
+	outer:
 		push cx
-		mov cx, 10
-		mov si, 0
-		mov max, -128
+		mov mindif, 127
+		mov edi, 0
+		mov ecx, 30
 
-		cycle1:
-			mov al, a1[si]
-			cmp max, al
-			jge next
-			mov max, al
-			mov di, si
+		inner:
+			mov al, FirstArr[esi]
+			sub al, SecondArr[edi]
 
-			next:
-				inc si
-				loop cycle1
+			cmp al, 0
+			jg greater
+			neg al
 
-		mov si, bx
-		mov a1[di], -128
-		inc di
-		mov ax, di
-		add si, si
-		mov r[si], ax
-		inc bx
+			greater: cmp al, mindif
+			jge skip
+			mov mindif, al
+
+			skip:
+			inc edi
+		loop inner
+
+		mov al, mindif
+		mov ResultArr[esi], al
+		inc esi
+
 		pop cx
-		loop cycle
+	loop outer
 
 	INVOKE ExitProcess, 0
 _main ENDP
